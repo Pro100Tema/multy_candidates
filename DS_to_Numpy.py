97250d564292b612ffa89250e6f7122b8b8359f4
Pro100Tema <40834728+Pro100Tema@users.noreply.github.com>
2022-08-03 01:19:48 +0300

    Update DS_to_Numpy.py

--- DS_to_Numpy.py
+++ DS_to_Numpy.py
@@ -1,6 +1,12 @@
 import ROOT 
 import numpy as np
 
+# input: 
+# dataset - initial dataset
+# var_lst - name of variables to add in numpy array 
+#ds = DS_to_Numpy(data, ['evt', 'run'])
+# output: 
+# data - numpy array with values of the required variables
 
 def DS_to_Numpy(dataset, var_lst):
     store = dataset.store()
@@ -14,4 +20,5 @@ def DS_to_Numpy(dataset, var_lst):
     for x in array_info.reals:
         if x.name in var_lst:
             data[x.name] = np.frombuffer(x.data, dtype=np.float64, count=array_info.size)
+    
     return data
