d07a40af0a15fe8b68ae933f85c13fbc7d482eab
Pro100Tema <40834728+Pro100Tema@users.noreply.github.com>
2022-08-04 03:26:15 +0300

    Using numpy structed array for saving data

--- DS_to_Numpy.py
+++ DS_to_Numpy.py
@@ -9,16 +9,19 @@ import numpy as np
 # data - numpy array with values of the required variables
 
 def DS_to_Numpy(dataset, var_lst):
+
     store = dataset.store()
-    data = {}
+    array_info = store.getArrays()
+    n = array_info.size
 
     if not isinstance(store, ROOT.RooVectorDataStore):
         dataset.ConvertToVectorStore()
 
-    array_info = store.getArrays()
+    # using numpy structed array
+    data = np.zeros(n, dtype={'names': (var_lst), 'formats':('f8', 'f8', (n ,len(var_lst)))})
 
     for x in array_info.reals:
         if x.name in var_lst:
-            data[x.name] = np.frombuffer(x.data, dtype=np.float64, count=array_info.size)
-    
+             data[x.name] = np.frombuffer(x.data, dtype = np.float64, count = n)
+
     return data
