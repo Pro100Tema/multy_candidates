c7a6289a0b990c64b2725895240dcb858d3caac2
Pro100Tema <40834728+Pro100Tema@users.noreply.github.com>
2022-09-07 15:55:07 +0300

    DS_to_Numpy for old ROOT version

--- DS_to_Numpy.py
+++ DS_to_Numpy.py
@@ -5,8 +5,27 @@ import numpy as np
 # dataset - initial dataset
 # var_lst - name of variables to add in numpy array 
 #ds = DS_to_Numpy(data, ['evt', 'run'])
+#ds = DS_to_Numpy_for_old_version(data, ['evt', 'run']) - for old ROOT package version
 # output: 
 # data - numpy array with values of the required variables
+
+
+def DS_to_Numpy_for_old_version(dataset, var_lst):
+    store = dataset.store()
+    n = store.size()
+    array_info = store.getBatches(0, n)
+
+    if not isinstance(store, ROOT.RooVectorDataStore):
+        dataset.ConvertToVectorStore()
+
+    # using numpy structed array
+    data = np.zeros(n, dtype={'names': (var_lst), 'formats':('f8', 'f8', (n ,len(var_lst)))})
+
+    for x in array_info:
+        if x.first in var_lst:
+             data[x.first] = np.frombuffer(x.data, dtype = np.float64, count = n)
+    return data
+
 
 def DS_to_Numpy(dataset, var_lst):
 
@@ -24,4 +43,8 @@ def DS_to_Numpy(dataset, var_lst):
         if x.name in var_lst:
              data[x.name] = np.frombuffer(x.data, dtype = np.float64, count = n)
 
+    for x in array_info.cats:
+        if x.name in var_lst:
+             data[x.name] = np.frombuffer(x.data, dtype = np.int32, count = n)
+                
     return data
